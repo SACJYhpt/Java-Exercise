cf67df8319af13603e5c21a85de946011e600c52
SACJYhpt <jerry10710@gmail.com>
2026-04-22 14:19:04 +0800

    Create FirstClass4Team.java

---
+++ FirstClass4Team.java
@@ -0,0 +1,6 @@
+public class FirstClass4Team {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習Github的程式!");
+        System.out.println("\"學號：\"01357120\"\n姓名：\"黃俊捷\"");
+    }
+}
